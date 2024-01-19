66daea94e63558465ffb2e606c4d98363eba4080
Pratik Paduri <87135097+hilobot4106@users.noreply.github.com>
2024-01-19 16:02:56 -0600

    made a door

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    key = game.askForNumber("what key")
+    if (key == 125) {
+        tiles.setWallAt(tiles.getTileLocation(7, 17), false)
+        tiles.setWallAt(tiles.getTileLocation(8, 17), false)
+        tiles.setTileAt(tiles.getTileLocation(7, 16), sprites.castle.tilePath4)
+        tiles.setTileAt(tiles.getTileLocation(8, 16), sprites.castle.tilePath6)
+    }
+})
+let key = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 let player1 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -17,28 +27,6 @@ let player1 = sprites.create(img`
     . . . . . . . . f f . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-let player2 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 2 2 2 . . . . . . . . 
-    . . . . 2 . . . 2 2 . . . . . . 
-    . . . . 2 . . . . . 2 . . . . . 
-    . . . . 2 . . . . . 2 . . . . . 
-    . . . . . 2 . . . 2 2 . . . . . 
-    . . . . . . 2 2 2 . . . . . . . 
-    . . . . . . 2 2 . . . . . . . . 
-    . . . . 2 . . 2 . . . 2 2 . . . 
-    . . . . . 2 . 2 . 2 2 . . . . . 
-    . . . . . . 2 2 2 . . . . . . . 
-    . . . . . . . 2 . . . . . . . . 
-    . . . . . . . 2 . . . . . . . . 
-    . . . . . . 2 2 2 . . . . . . . 
-    . . . . . 2 2 . 2 . . . . . . . 
-    `, SpriteKind.Player)
-controller.player1.moveSprite(player1)
-controller.player2.moveSprite(player2)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, player1)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, player2)
-player1.setPosition(0, 25)
-player2.setPosition(0, 25)
 scene.cameraFollowSprite(player1)
+player1.setPosition(0, 25)
+controller.moveSprite(player1)
